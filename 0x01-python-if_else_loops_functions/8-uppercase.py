01147375317f1241c47fb36d65090b10eafa95f8
Lakhanya <lakhanya.mahlati@gmail.com>
2022-06-28 19:50:43 -0700

    Indentation is a ...

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for c in str:
         if ord("a") <= ord(c) <= ord("z"):
-	    c = chr(ord(c) + (ord("A") - ord("a")))
-	print("{:s}".format(c), end="")
+            c = chr(ord(c) + (ord("A") - ord("a")))
+        print("{:s}".format(c), end="")
     print("")
